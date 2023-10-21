1102fbe49c8c8bec39f3910e30f97da2e79753c0
Ganoben <ganonben@gmail.com>
2023-10-21 14:23:13 +0300

    Quartz sync: Oct 21, 2023, 2:23 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,11 +27,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
